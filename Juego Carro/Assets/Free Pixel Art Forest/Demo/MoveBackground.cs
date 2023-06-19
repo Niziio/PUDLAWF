0ff4daa5157f1d2a8f3be93d8f944a4fb36b5abe
joda1999 <135388618+joda1999@users.noreply.github.com>
2023-06-19 00:15:09 -0500

    inicio (#1)
    los assets para inicio

---
+++ Juego Carro/Assets/Free Pixel Art Forest/Demo/MoveBackground.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBackground : MonoBehaviour 
+{
+	private float dura, empieza;
+	public float parallax;
+	public GameObject camara;
+
+	void Start () 
+	{
+		empieza = transform.position.x;
+		dura = GetComponent<SpriteRenderer>().bounds.size.x;
+
+	}
+	
+	void FixedUpdate () 
+	{
+		float temp = (camara.transform.position.x * (1 - parallax));
+		float dist = (camara.transform.position.x * parallax);
+
+		transform.position = new Vector3(empieza + dist, transform.position.y, transform.position.z);
+
+		if (temp > empieza + dura)
+			empieza += dura;
+		else if (temp < empieza - dura) 
+			empieza -= dura;
+
+	}
+}
